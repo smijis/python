6196826d29123a705f3dec95f1a9bd045d7a87ea
smijis <shinselina@hotmail.com>
2025-11-18 19:55:56 +1100

    variables part 1

---
+++ test.py
@@ -0,0 +1,8 @@
+name="Oliver"
+print(f"Hello {name}!")
+
+my_favourite_number = 3
+times_five = my_favourite_number * 5
+
+print("I multiplied your favourite number by 5 and got...")
+print(times_five)
